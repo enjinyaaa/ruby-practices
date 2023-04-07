e4fbc2cf5b4d14a9dfb28d1ab1c9d8f168df238a
enjinyaaa <katocchi3@gmail.com>
2023-04-07 22:41:07 +0900

    Implement FizzBuzz program

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,16 @@
+def fizzbuzz(cntnum, num1, num2)
+  cntnum.times.each do |i|
+    num = i + 1
+    if num % num1 == 0 && num % num2 == 0
+      puts "FizzBuzz"
+    elsif num % num1 == 0
+      puts "Fizz"
+    elsif num % num2 == 0
+      puts "Buzz"
+    else
+      puts num
+    end
+  end
+end
+
+fizzbuzz(20, 3, 5)
