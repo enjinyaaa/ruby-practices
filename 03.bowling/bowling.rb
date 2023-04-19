ab0d6daa1b5ceaf16748109adf3ee09ab92c7899
enjinyaaa <katocchi3@gmail.com>
2023-04-19 22:48:03 +0900

    Modified redundant program v3

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,22 +1,17 @@
 # frozen_string_literal: true
 
 def point_calc(point, first_add_times, second_add_times)
-  retval = point * (first_add_times + 1)
-  first_add_times = second_add_times
-  second_add_times = 0
-  [retval, first_add_times, second_add_times]
+  [point * (first_add_times + 1), second_add_times, 0]
 end
 
-def judge_add_times_strike(first_add_times)
-  tmp_first_add_times = first_add_times + 1
-  [tmp_first_add_times, 1]
+def judge_add_times_strike(p_str, first_add_times)
+  return [first_add_times + 1, 1] if p_str == 'X'
+
+  [first_add_times, 0]
 end
 
-def judge_add_times(frame_points, frame)
-  tmp_first_add_times = frame_points.sum == 10 && frame != 10 ? 1 : 0
-  tmp_second_add_times = 0
-  tmp_frame = frame + 1
-  [tmp_first_add_times, tmp_second_add_times, [], tmp_frame]
+def judge_add_times_spare(frame_points)
+  [frame_points.sum == 10 ? 1 : 0, 0]
 end
 
 def score_calc
@@ -27,18 +22,20 @@ def score_calc
     point = p_str == 'X' ? 10 : p_str.to_i
     point_val, first_add_times, second_add_times = point_calc(point, first_add_times, second_add_times)
     score += point_val
+    next if frame == 10
 
-    if p_str == 'X'
-      if frame != 10
-        first_add_times, second_add_times = judge_add_times_strike(first_add_times)
-        frame += 1
-      end
+    first_add_times, second_add_times = judge_add_times_strike(p_str, first_add_times)
+    if second_add_times == 1
+      frame += 1
       next
     end
+
     frame_points << point
     next unless frame_points.size == 2
 
-    first_add_times, second_add_times, frame_points, frame = judge_add_times(frame_points, frame)
+    first_add_times, second_add_times = judge_add_times_spare(frame_points)
+    frame_points.clear
+    frame += 1
   end
   puts score
 end
