52e2724e4842c48b5497aaea8c31b153c454f671
enjinyaaa <katocchi3@gmail.com>
2023-04-21 22:20:45 +0900

    Revert adding game_points, and modified redundant program v5

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -14,38 +14,33 @@ def judge_add_times_spare(frame_points)
   [frame_points.sum == 10 ? 1 : 0, 0]
 end
 
-def arrays_clear(frame_points, frame_calc_array)
-  frame_points.clear
-  frame_calc_array.clear
+def frame_end_process(frame)
+  [frame + 1, []]
 end
 
 def score_calc
-  first_add_times = second_add_times = 0
+  score = first_add_times = second_add_times = 0
+  frame = 1
   frame_points = []
-  frame_calc_array = []
-  game_points = []
   ARGV[0].split(',').each do |p_str|
     point = p_str == 'X' ? 10 : p_str.to_i
     frame_points << point
     calc_point, first_add_times, second_add_times = point_calc(point, first_add_times, second_add_times)
-    frame_calc_array << calc_point
-    next if game_points.size + 1 == 10
+    score += calc_point
+    next if frame == 10
 
     first_add_times, second_add_times = judge_add_times_strike(frame_points, first_add_times)
     if frame_points.first == 10
-      game_points << frame_calc_array.dup
-      arrays_clear(frame_points, frame_calc_array)
+      frame, frame_points = frame_end_process(frame)
       next
     end
 
     next if frame_points.size < 2
 
     first_add_times, second_add_times = judge_add_times_spare(frame_points)
-    game_points << frame_calc_array.dup
-    arrays_clear(frame_points, frame_calc_array)
+    frame, frame_points = frame_end_process(frame)
   end
-  game_points << frame_calc_array.dup
-  puts game_points.flatten.sum
+  puts score
 end
 
 score_calc
