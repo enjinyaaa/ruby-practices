a7f60b9c2c58b693f350bc731409c8d18a3d4050
enjinyaaa <katocchi3@gmail.com>
2023-05-10 20:53:41 +0900

    Fixed unsafe process

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -18,15 +18,15 @@ def format_output_strings(filenames)
   max_lengths_by_col = columns.map { |ary| ary.max_by(&:length)&.length }
   Array.new(rows_num) do |row_i|
     row = columns.filter_map.with_index do |column, col_i|
-      column[row_i]&.ljust(max_lengths_by_col[col_i])
+      column[row_i]&.ljust(max_lengths_by_col[col_i]) unless max_lengths_by_col[col_i].nil?
     end
     row.join('  ').rstrip
   end
 end
 
 def ls(args = ARGV)
-  filepath = args[0].nil? ? '.' : args[0]
-  filenames = Dir.glob("#{filepath}/*").map { |path| File.basename(path) }
+  filepath = args[0] || '.'
+  filenames = Dir.glob(File.join(filepath, '/*')).map { |path| File.basename(path) }
   puts format_output_strings(filenames)
 end
 
