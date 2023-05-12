2a009326b988d6cd163a8813f8ec8f8d8a34be1e
enjinyaaa <katocchi3@gmail.com>
2023-05-12 21:42:43 +0900

    Modified redandant process

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -15,19 +15,16 @@ end
 
 def format_output_strings(filenames)
   rows_num = (filenames.length.to_f / COLUMN_MAX).ceil
-  return nil if rows_num.zero?
-
-  columns = filenames.each_slice(rows_num).map { |a| a }
-  max_lengths_by_col = columns.map { |ary| ary.max_by(&:length)&.length }
-  rows = columns.map { |a| a + Array.new(rows_num - a.size, nil) }.transpose
+  columns = filenames.each_slice(rows_num).to_a
+  max_lengths_by_col = columns.map { |col| col.max_by(&:length)&.length }
+  rows = columns.map { |col| col + Array.new(rows_num - col.size, nil) }.transpose
   join_strings_by_row(rows, max_lengths_by_col)
 end
 
 def ls(args = ARGV)
   filepath = args[0] || '.'
   filenames = Dir.glob(File.join(filepath, '*')).map { |path| File.basename(path) }
-  output_strings = format_output_strings(filenames)
-  puts output_strings unless output_strings.nil?
+  puts format_output_strings(filenames) unless filenames.empty?
 end
 
 ls if __FILE__ == $PROGRAM_NAME
