2ef8b6c8f6b7693c60c21776ae9b606caee8f75f
enjinyaaa <katocchi3@gmail.com>
2023-05-30 21:19:21 +0900

    Modified reassignment of "filenames" variable

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -30,18 +30,29 @@ def parse_args_option(args)
   [args, options]
 end
 
+def option_a_process(filenames, options)
+  if !options[:a]
+    filenames.reject { |fname| fname.start_with?('.') } unless options[:a]
+  else
+    filenames
+  end
+end
+
+def option_r_process(filenames, options)
+  if options[:r]
+    filenames.reverse
+  else
+    filenames
+  end
+end
+
 def ls(args = ARGV)
   parsed_args, options = parse_args_option(args)
   filepath = parsed_args[0] || '.'
   filenames = Dir.entries(filepath).sort
-  filenames = filenames.reject { |fname| fname.start_with?('.') } unless options[:a]
-  filenames = filenames.reverse if options[:r]
-  "#{format_output_strings(filenames).join("\n")}\n" unless filenames.empty?
+  option_a_processed_fnames = option_a_process(filenames, options)
+  option_r_processed_fnames = option_r_process(option_a_processed_fnames, options)
+  "#{format_output_strings(option_r_processed_fnames).join("\n")}\n" unless option_r_processed_fnames.empty?
 end
 
-def ls_main(args = ARGV)
-  output_strings = ls(args)
-  print output_strings
-end
-
-ls_main if __FILE__ == $PROGRAM_NAME
+print ls(ARGV) if __FILE__ == $PROGRAM_NAME
